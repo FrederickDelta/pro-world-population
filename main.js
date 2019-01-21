4d7c1bd85a2a5d89804251b7e6864f71344151ec
Frederick D <FrederickDelta@gmail.com>
2019-01-21 17:25:46 +0800

    Add search function

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const countries = []
 fetch(endpoint)
   .then(res => res.json())
   .then(data => countries.push(...data))
+
+function search(keyword, countries){
+  return countries.filter(country => {
+    const regex = new RegExp(keyword, 'gi')
+    return country.country.match(regex)
+  })
+}
